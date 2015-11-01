4549b6bf5a069ef1ba8a0f1dab0be0c2a1b18566
Martin Relevo <martin.relevo@hotmail.co.uk>
2015-11-01 21:46:08 +0000

    Menu done and connected to the intro

---
+++ Stab at Stab Town U.S.Stab/Assets/Stabby Assets/Menu/Assets/Scripts/changescene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changescene : MonoBehaviour {
+
+	// Use this for initialization
+	public void Changescene (int changescene)
+	{
+		Application.LoadLevel (changescene);
+		Debug.Log ("Hey");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Stab at Stab Town U.S.Stab/Assets/Stabby Assets/Menu/Assets/Scripts/exitgame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exitgame : MonoBehaviour
+{
+
+	// Use this for initialization
+	public void Quit ()
+	{
+		Application.Quit ();
+		Debug.Log ("Quit");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
