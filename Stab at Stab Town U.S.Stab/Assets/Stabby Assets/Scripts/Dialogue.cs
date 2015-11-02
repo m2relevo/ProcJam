dcdf16aeadfdc2e76e22e15dd8b38957459f7c26
mp2-cole <colemarcus@hotmail.co.uk>
2015-11-02 10:56:19 +0000

    Scene
    wrrohhjr

--- Stab at Stab Town U.S.Stab/Assets/Stabby Assets/Scripts/Dialogue.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Dialogue : MonoBehaviour 
-{
-	public string Attribute = " is letting armpits loose in a tank-top";
-	public string Evidence = "Armpit Hair was found on the body";
-
-}
