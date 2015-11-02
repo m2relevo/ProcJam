09877a5ec7f8b852e1aa00aef2a557a982c13b35
mp2-cole <colemarcus@hotmail.co.uk>
2015-11-02 11:29:12 +0000

    Revert "Scene"
    This reverts commit dcdf16aeadfdc2e76e22e15dd8b38957459f7c26.

---
+++ Stab at Stab Town U.S.Stab/Assets/Stabby Assets/Scripts/Dialogue.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour 
+{
+	public string Attribute = " is letting armpits loose in a tank-top";
+	public string Evidence = "Armpit Hair was found on the body";
+
+}
